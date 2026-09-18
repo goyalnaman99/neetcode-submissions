435cc915be9b6fdd2b43604ea1fea73d7ae23857
Naman Goyal <goyalnaman@outlook.com>
2026-09-18 21:56:28 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let map = {};
+        for (let i = 0; i < nums.length; i++) {
+            if(!Object.hasOwn(map, target - nums[i])) map[nums[i]] = i;
+            else return [map[target - nums[i]], i];
+        }
+    }
+}
