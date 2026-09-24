9a1f8fdc292c8352ea9d6e0a3ee035541d5bf29b
Naman Goyal <goyalnaman@outlook.com>
2026-09-25 01:32:59 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        if(!numbers.length) return [];
+        let i = 0; 
+        let j = numbers.length - 1;
+        while(i < j){
+            if(numbers[i] + numbers[j] > target) j--;
+            else if (numbers[i] + numbers[j] < target) i++;
+            else return [i+1, j+1];
+        }
+        return [];
+    }
+}
